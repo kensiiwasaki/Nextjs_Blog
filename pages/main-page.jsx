1ec8f4ceb6c29290b6b8903c23bd109b0ecd1433
けんし <kensiiwasaki@gmail.com>
2022-06-18 02:25:23 +0900

    ログイン後のメインページ

---
+++ pages/main-page.jsx
@@ -0,0 +1,36 @@
+import Cookie from "universal-cookie";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
+
+const cookie = new Cookie();
+
+export default function MainPage() {
+  const router = useRouter();
+  // ログアウトする関数
+  const logout = () => {
+    // removeでcookieの中身を削除
+    cookie.remove("access_token", { path: "/" });
+    // indexのページに遷移
+    router.push("/");
+  };
+
+  return (
+    <Layout title="Main page">
+      <svg
+        onClick={logout}
+        xmlns="http://www.w3.org/2000/svg"
+        className=" mt-10 cursor-pointer h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+        />
+      </svg>
+    </Layout>
+  );
+}
