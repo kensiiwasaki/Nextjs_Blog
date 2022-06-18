82b4dbf63a5ef9ca981f9ef88ccdc57abe24ffbe
けんし <kensiiwasaki@gmail.com>
2022-06-18 19:57:33 +0900

    タスクページの作成(useSWR)

--- pages/task-page.jsx
+++ pages/task-page.jsx
@@ -1,9 +1,40 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
+import { getAllTasksData } from "../lib/tasks";
+import Task from "../components/Task";
+import useSWR from "swr";
+import { useEffect } from "react";
 
-export default function TaskPage() {
+// クライアントのフェッチ用の関数
+const fetcher = (url) => fetch(url).then((res) => res.json());
+// apiのエンドポイントを定数として定義
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+export default function TaskPage({ staticfilterdTasks }) {
+  // 第一引数にfetchしたいURLを渡す
+  // 第二引数にfetcher関数を渡す
+  // 第三引数にfallbackDataを指定できる
+  // 返り値dataをtasksという名前で受け取る
+  // mutate関数 データのキャッシュを最新のものに更新してくれる
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticfilterdTasks,
+  });
+  // tasksが存在する場合にソートする
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+
+  // taskーpageがマウントされた時に確実にキャッシュの内容が更新されるようにuseEffectを使用
+  useEffect(() => {
+    mutate();
+  }, []);
+
   return (
     <Layout title="Task page">
+      <ul>
+        {filteredTasks &&
+          filteredTasks.map((task) => <Task key={task.id} task={task} />)}
+      </ul>
       <Link href="/main-page">
         <div className=" flex cursor-pointer mt-12">
           <svg
@@ -25,4 +56,13 @@ export default function TaskPage() {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const staticfilterdTasks = await getAllTasksData();
+
+  return {
+    props: { staticfilterdTasks },
+    revalidate: 3,
+  };
 }
